e531f56ff5a56c7eb904a6f3b0ca4522d09888c6
Tommi Lepola <lepola.tommi@gmail.com>
2022-01-23 21:11:52 +0200

    Fixes to routing

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,11 +1,13 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8081/'
+  },
   devServer: {
     port: 8081,
     historyApiFallback: {
